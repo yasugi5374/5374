cae29a88d4c69882e75d0f6baa8500f52ede68aa
matsueT5374 <mishima001@users.noreply.github.com>
2019-03-29 15:34:15 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -1,14 +1,14 @@
 /*
  * 5374 setting
+ *
 */
+
 var SVGLabel = false; // SVGイメージを使用するときは、true。用意できない場合はfalse。
+
 var MaxDescription = 9; // ごみの最大種類、９を超えない場合は変更の必要はありません。
+
 var MaxMonth = 3;
 
-var KoteiKN = false;    // 固定の休止期間１２月３０日～１月３日を使用するときはtrue。
-
-var WeekShiftA = true; // 休止期間なら週をずらすときは、true。Ａ地区
-var WeekShiftB = true;  // 休止期間なら週をずらすときは、true。Ｂ地区
+var WeekShift = true; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
 
 var SkipSuspend = true; // 休止期間を除去するときは、true。奈良の仕様は、true。
-
